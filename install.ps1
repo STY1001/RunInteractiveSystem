b204b603eec4b5def1e9363a8a104ac7c8380160
STY1001 <57889852+STY1001@users.noreply.github.com>
2024-04-08 23:34:50 +0200

    Fix

--- install.ps1
+++ install.ps1
@@ -1,5 +1,5 @@
 if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-    Write-Host "Please run this script as an administrator."
+    Write-Host "Please run this script as admin."
     Exit
 }
 $zipurl = 'https://github.com/STY1001/RunInteractiveSystem/releases/latest/download/RunInteractiveSystem.zip'
@@ -11,3 +11,4 @@ if (Test-Path $modulepath) {
 Expand-Archive -Path "$env:TEMP\RunInteractiveSystem.zip" -DestinationPath $modulepath
 Remove-Item -Path "$env:TEMP\RunInteractiveSystem.zip" -Force
 Import-Module RunInteractiveSystem
+Write-Host "RunInteractiveSystem has been installed."
